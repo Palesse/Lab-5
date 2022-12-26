d172f448d7535244107a12a7a54b771470744c89
Palesse <113944611+Palesse@users.noreply.github.com>
2022-12-26 09:07:53 +0300

    Create arrays.h

---
+++ Ex-2/arrays.h
@@ -0,0 +1,8 @@
+#pragma once
+
+void check(int* k);
+void arr_memory_allocate(int row, int col, int*** arr);
+void input_arr(int row, int*** arr);
+void output_arr(int row, int*** arr);
+void arr_free(int row, int*** arr);
+void arr_job(int row, int*** arr);
